2e924942d13f323d11394c45cd2a671bc43397cc
Jawhar Chouri <djochouri@gmail.com>
2023-12-22 11:08:20 +0100

    TodoList component ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
-
+import AddIcon from '@mui/icons-material/Add';
+import ToDoList from './components/toDoList';
 import './App.css'
 
 function App() {
   
   return (
-    <div className=''>
-     
-      aaaa
+    <div className='mx-auto max-w-full px-4 sm:px-6 lg:px-8 w-full md:max-w-3xl'>
+      <div className='my-12'>
+      <div className='flex items-center justify-between'>
+        <h2 className='text-0a1629 text-4xl font-bold'>Task List</h2>
+        <button className='bg-[#713fff] text-white rounded-lg shadow-md hover:shadow-lg focus:outline-none px-8 py-3 font-semibold cursor-pointer text-base '>
+          
+            <AddIcon />
+
+
+          
+          Add Task
+        </button>
+
+      </div>
+      
+
+      <div className='my-12'>
+        <ToDoList/>
+        <ToDoList/>
+        <ToDoList/>
+        <ToDoList/>
+        <ToDoList/>
+        
+        
+
+      </div>
+      </div>
     </div>
   )
 }
